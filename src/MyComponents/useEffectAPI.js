1a7474204e4cfcad4f704d54ebe953f845a8194f
Zain Tariq <bsce19023@itu.edu.pk>
2022-09-14 03:07:54 +0500

    update

--- src/MyComponents/useEffectAPI.js
+++ src/MyComponents/useEffectAPI.js
@@ -26,10 +26,10 @@ const UseEffectAPI = (props) => {
             <div className="card">
               <div className="card-body">
                 <button type="button" className="btn-close" aria-label="Close"></button>
-                <h5 className="card-title">Name: {data.results[0].name.first}</h5>
+                {/* <h5 className="card-title">Name: {data.results[0].name.first}</h5>
                 <p className="card-text">Email: {data.results[0].email}</p>
                 <p className="card-text">Cell: {data.results[0].cell}</p>
-                <p className="card-text">Location: {data.results[0].location.city}</p>
+                <p className="card-text">Location: {data.results[0].location.city}</p> */}
                 <a href="#" className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Details</a>
               </div>
             </div>
